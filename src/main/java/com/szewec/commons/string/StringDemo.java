bb5fcda0b8bbdf9481913efdf80994d54aeee6c0
by19880401 <willis.bai@outlook.com>
2020-07-21 20:01:16 +0800

    create a simple structure

---
+++ src/main/java/com/szewec/commons/string/StringDemo.java
@@ -0,0 +1,11 @@
+package com.szewec.commons.string;
+
+import org.apache.commons.lang.StringUtils;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        String str1 = "01";
+        String str2 = "02";
+        System.out.println(StringUtils.equals(str1, str2));
+    }
+}
