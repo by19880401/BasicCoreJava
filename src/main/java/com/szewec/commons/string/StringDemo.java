63e668131c741334718f1dc77ce14440c72e0a56
by19880401 <willis.bai@outlook.com>
2020-07-23 20:43:59 +0800

    Check-in another case

--- src/main/java/com/szewec/commons/string/StringDemo.java
+++ src/main/java/com/szewec/commons/string/StringDemo.java
@@ -6,6 +6,9 @@ public class StringDemo {
     public static void main(String[] args) {
         String str1 = "01";
         String str2 = "02";
+        String str3 = "";
+        String str4 = null;
         System.out.println(StringUtils.equals(str1, str2));
+        System.out.println(StringUtils.equals(str3, str4));
     }
 }
