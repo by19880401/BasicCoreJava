60b34531f58e6be0db53bb798316a12d7f7de216
by19880401 <willis.bai@outlook.com>
2021-08-11 09:50:08 +0800

    设计模式之命令模式demo

--- src/main/java/com/example/designpartten/commandpartten/DanceReceiver.java
+++ src/main/java/com/example/designpartten/commandpartten/DanceReceiver.java
@@ -3,8 +3,8 @@ package com.example.designpartten.commandpartten;
 import cn.hutool.log.StaticLog;
 
 /**
- * 接收者，真正接受并执行命令的对象
- * 这里执行者接到唱歌的命令后，开始【跳舞】的动作
+ * 接收者（即电视），真正接受并执行命令的对象
+ * 这里执行者接到【跳舞的命令】后，开始【跳舞】的动作
  *
  * @author bai.yang email:willis.bai@outlook.com
  * @date 2021/8/10
