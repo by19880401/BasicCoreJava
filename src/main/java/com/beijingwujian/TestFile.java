399cdf25dea49f8c5e3e2d34933b822788100ea8
WillisBai <willis.bai@outlook.com>
2023-10-24 10:11:40 +0800

    change the comments
    Signed-off-by: WillisBai <willis.bai@outlook.com>

--- src/main/java/com/beijingwujian/TestFile.java
+++ src/main/java/com/beijingwujian/TestFile.java
@@ -1,7 +1,9 @@
 package com.beijingwujian;
 
+import cn.hutool.log.StaticLog;
+
 public class TestFile {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("This is a test to check if the code can be promoted to GitHub by TortoiseGit Tool.");
     }
 }
