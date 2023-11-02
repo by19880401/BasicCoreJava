51971c7bc6de69daa485d1ab93323bb44f26dc81
WillisBai <willis.bai@outlook.com>
2023-11-02 16:17:45 +0800

    add mockit demo
    Signed-off-by: WillisBai <willis.bai@outlook.com>

---
+++ src/main/java/com/objectiva/mockito/Person.java
@@ -0,0 +1,51 @@
+package mockito;
+
+import cn.hutool.log.StaticLog;
+
+public class Person {
+
+    /**
+     * 根据输入的类型判断是人的性别
+     *
+     * @param type
+     */
+    public void getSex(int type) {
+        if (type == 0) {
+            StaticLog.info("type是{}, 女的", type);
+        } else if (type == 1) {
+            StaticLog.info("type是{}, 男的", type);
+        } else {
+            StaticLog.info("类型错误!");
+        }
+    }
+
+    /**
+     * 根据输入的类型判断是人的性别
+     *
+     * @param type
+     * @return
+     */
+    public String getSexAsString(int type) {
+        if (type == 0) {
+            StaticLog.info("type是{}, 女的", type);
+            return "Female";
+        } else if (type == 1) {
+            StaticLog.info("type是{}, 男的", type);
+            return "Male";
+        } else {
+            StaticLog.info("类型错误!");
+            return "Null";
+        }
+    }
+
+    /**
+     * 根据输入的类型判断是否为男性
+     *
+     * @param type
+     * @return
+     */
+    public boolean isMan(int type) {
+        return type == 1;
+    }
+
+}
---
+++ src/main/java/com/objectiva/mockito/RandomInt.java
@@ -0,0 +1,25 @@
+package mockito;
+
+import java.util.Random;
+
+public class RandomInt {
+
+    /**
+     * 无参
+     *
+     * @return
+     */
+    public int generateRandomValue() {
+        return new Random().nextInt();
+    }
+
+    /**
+     * 有参
+     *
+     * @param bound
+     * @return
+     */
+    public int generateRandomValue(int bound) {
+        return new Random().nextInt(bound);
+    }
+}
