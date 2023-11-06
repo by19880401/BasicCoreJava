fd1e3076280fa753038072ed2b69dd6d0dc3e2c5
WillisBai <willis.bai@outlook.com>
2023-11-06 09:14:28 +0800

    demo optional

---
+++ src/main/java/com/objectiva/Optional/OptionalNullDemo.java
@@ -0,0 +1,18 @@
+package com.objectiva.Optional;
+
+import cn.hutool.log.StaticLog;
+
+import java.util.Optional;
+
+public class OptionalNullDemo {
+    public static void main(String[] args) {
+//        testOptional(Optional.of("123"));// has value
+        testOptional(null);// has no value
+    }
+
+
+    private static void testOptional(Optional<String> param){
+        Optional<String> opt = Optional.ofNullable(param).orElse(Optional.empty());
+        StaticLog.info("reset quota limits by tenantId: {}.", opt.isPresent()?opt.get():null);
+    }
+}
