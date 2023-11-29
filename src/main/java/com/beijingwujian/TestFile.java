99b483b4118040eb8ed141b1d9b5f440416b7cd8
WillisBai <willis.bai@outlook.com>
2023-11-29 09:24:22 +0800

    also add comments

--- src/main/java/com/beijingwujian/TestFile.java
+++ src/main/java/com/beijingwujian/TestFile.java
@@ -5,5 +5,6 @@ import cn.hutool.log.StaticLog;
 public class TestFile {
     public static void main(String[] args) {
         System.out.println("This is a test to check if the code can be promoted to GitHub by TortoiseGit Tool.");
+        StaticLog.info("This is a test to check if the code can be promoted to GitHub by TortoiseGit Tool.");
     }
 }
