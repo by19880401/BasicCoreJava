00b89fbca077a2419b4d79f333eddf1bfc9f76e5
WillisBai <willis.bai@outlook.com>
2023-11-30 10:50:41 +0800

    also add comments

--- src/main/java/com/beijingwujian/TestFile.java
+++ src/main/java/com/beijingwujian/TestFile.java
@@ -2,6 +2,9 @@ package com.beijingwujian;
 
 import cn.hutool.log.StaticLog;
 
+/**
+ * 在BJWJ，基本上不写Java代码，所以基本上很少能有代码提交的
+ */
 public class TestFile {
     public static void main(String[] args) {
         System.out.println("This is a test to check if the code can be promoted to GitHub by TortoiseGit Tool.");
