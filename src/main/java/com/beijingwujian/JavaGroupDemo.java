9e48b7fa72f87eb73f47f4f32f9b8a81b9f810cb
WillisBai <willis.bai@outlook.com>
2023-12-13 17:47:58 +0800

    add test result

--- src/main/java/com/beijingwujian/JavaGroupDemo.java
+++ src/main/java/com/beijingwujian/JavaGroupDemo.java
@@ -6,6 +6,9 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * lamoda分组的便利性
+ */
 public class JavaGroupDemo {
     public static void main(String[] args) {
         List<User> list = new ArrayList<>();
