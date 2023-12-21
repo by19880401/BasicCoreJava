a4505cd28187fa3a42785c27251b31ea0417e8ce
WillisBai <willis.bai@outlook.com>
2023-12-21 13:50:18 +0800

    test log content

--- src/main/java/com/beijingwujian/JavaLogDemo.java
+++ src/main/java/com/beijingwujian/JavaLogDemo.java
@@ -11,20 +11,36 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class JavaLogDemo {
+    /**
+     * 主函数，每天执行它
+     *
+     * @param args
+     */
     public static void main(String[] args) {
+        // 获取当前时间
         String currentDate = getCurrentTimeStr();
+        // 把当前时间作为参数，获取当天的日志markdown文件
         ClassPathResource resource = new ClassPathResource("/log/" + currentDate + ".md");
         try {
+            // 获取MD文件
             String filePath = resource.getFile().getPath();
+            // 读取MD文件
             List<String> lines = Files.readAllLines(Paths.get(filePath));
+            // 一行一行遍历文件内容，打印
             for (String line : lines) {
                 StaticLog.info(line);
             }
         } catch (IOException e) {
+            // 如果发生异常，打印异常信息
             StaticLog.warn("some errors occur: {}", e.getMessage());
         }
     }
 
+    /**
+     * 格式化（当前）时间
+     *
+     * @return
+     */
     private static String getCurrentTimeStr() {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         return dtf.format(LocalDateTime.now());
