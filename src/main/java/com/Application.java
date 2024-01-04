0ce40e35b8ed18a81e978a58aa860caf11d08446
WillisBai <willis.bai@outlook.com>
2024-01-04 13:47:31 +0800

    改造spring boot项目

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,10 +1,13 @@
-package com.szewec;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 这其实是一个SpringBoot项目，但我按照Java Client项目在使
+ */
 @SpringBootApplication
 @RestController
 public class Application {
