660b248c4f3a947ff1847d1b65a5076204bbdd21
WillisBai <willis.bai@outlook.com>
2024-01-10 09:28:15 +0800

    adjust enum in order to allow the special file type

--- src/main/java/com/beijingwujian/log/FileType.java
+++ src/main/java/com/beijingwujian/log/FileType.java
@@ -1,19 +1,15 @@
 package com.beijingwujian.log;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+/**
+ * 支持的文件类型
+ */
+@ToString
+@AllArgsConstructor // 由于枚举类的构造方法强制为私有，所以@AllArgsConstructor不需要使用access = AccessLevel.PRIVATE
 public enum FileType {
     MARKDOWN(".md"), TEXT(".txt");
 
-    private String suffix;
-
-    FileType(String suffix) {
-        this.suffix = suffix;
-    }
-
-    public String getSuffix() {
-        return suffix;
-    }
-
-    public void setSuffix(String suffix) {
-        this.suffix = suffix;
-    }
+    private final String suffix;//拿掉setter，直接暴露属性，将属性改成final确保不会被篡改
 }
