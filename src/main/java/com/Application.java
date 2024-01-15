c6c704c2e2b084cf9a8ac3b21ab4b9c1386354cf
WillisBai <willis.bai@outlook.com>
2024-01-15 16:02:44 +0800

    代码提交

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * 这其实是一个SpringBoot项目，但我按照Java Client项目在使
  */
 @SpringBootApplication
-@RestController
+@RestController // 由于下方使用@RequestMapping("/index")以允许通过URL访问，所以这里使用@RestController来标明这是一个controller
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
