8b2edd3db363f8f6b6b071494ec7b6a06cfc01d6
WillisBai <willis.bai@outlook.com>
2024-01-25 11:08:20 +0800

    unused code

--- src/main/java/com/web/configuration/ApplicationYamlBean.java
+++ src/main/java/com/web/configuration/ApplicationYamlBean.java
@@ -2,9 +2,6 @@ package com.web.configuration;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
