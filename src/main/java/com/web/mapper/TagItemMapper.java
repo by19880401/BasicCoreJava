abd8318c1889681295cf837b23007f4d2fc30a24
WillisBai <willis.bai@outlook.com>
2024-02-19 16:27:35 +0800

    提交代码

--- src/main/java/com/web/mapper/TagItemMapper.java
+++ src/main/java/com/web/mapper/TagItemMapper.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.type.JdbcType;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,9 +16,9 @@ public interface TagItemMapper extends BaseMapper<TagsItem> {
 
     @Select("select * from tags_item where id=#{id}")
     @Results(id = "tagItemResultMap", value = {
-            @Result(id = true, column = "id", property = "id"),
+            @Result(id = true, column = "id", property = "id"),//是否主键
             @Result(column = "label", property = "label"),
-            @Result(column = "f_tag_id", property = "tagId")
+            @Result(column = "f_tag_id", property = "tagId",jdbcType = JdbcType.VARCHAR)//FIXME don't know why there is no value
     })
     List<TagsItem> getDetailListById(@Param("id") Long id);
 }
